dc93c71566c7f23142e0153ff842900374190c94
hakluke <13975395+hakluke@users.noreply.github.com>
2021-08-09 09:50:29 +1000

    added error checks, moved regex compile out of loop, removed unnecessary map ptr

--- haklistgen.go
+++ haklistgen.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"strings"
@@ -12,13 +13,16 @@ import (
 func main() {
 	list := make(map[string]struct{}) // store the output lines to check for dupes
 	s := bufio.NewScanner(os.Stdin)
+	r := regexp.MustCompile(`[a-zA-Z0-9\.\-\_\/]*`)
 	for s.Scan() {
-		process(s.Text(), &list)
+		process(s.Text(), list, r)
+	}
+	if s.Err() != nil {
+		log.Printf("Error: %s\n", s.Err())
 	}
 }
 
-func process(text string, list *map[string]struct{}) {
-	r := regexp.MustCompile(`[a-zA-Z0-9\.\-\_\/]*`)
+func process(text string, list map[string]struct{}, r *regexp.Regexp) {
 	for _, match := range r.FindAllString(text, -1) {
 		if match != "" {
 			if strings.Contains(match, ".") {
@@ -32,7 +36,7 @@ func process(text string, list *map[string]struct{}) {
 	}
 }
 
-func splitBy(text string, split string, list *map[string]struct{}) {
+func splitBy(text string, split string, list map[string]struct{}) {
 	splitString := strings.Split(text, split)
 	for _, match := range splitString {
 		if match != "" {
@@ -53,9 +57,8 @@ func removeSlashPrefix(text string) string {
 	return text
 }
 
-func printIfUnique(text string, listPtr *map[string]struct{}) {
+func printIfUnique(text string, list map[string]struct{}) {
 	text = removeSlashPrefix(text)
-	list := *listPtr
 	if _, ok := list[text]; !ok {
 		fmt.Println(text)
 		list[text] = struct{}{}
